f0a11a819b00ca88512232896c032ec911596ec3
Mike Carcasole <mikecarcasole@gmail.com>
2022-12-24 09:00:37 -0500

    Get mention detection together

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import { useMemo, useState } from "react";
 import NoSSR from "react-no-ssr";
+import { BaseRange, Editor, Range, Transforms } from "slate";
 import { ReactEditor, Slate } from "slate-react";
 import {
   editorDataToMarkdown,
@@ -30,31 +31,75 @@ Nice!
 
   const editor = useMarkdownEditable();
 
+  const [mentionQuery, setMentionQuery] = useState<{
+    search: string;
+    range: BaseRange;
+  }>();
+  const getMentionQueryFromEditor = () => {
+    const { selection } = editor;
+    if (!selection) return;
+    if (!Range.isCollapsed(selection)) return;
+
+    const [cursor] = Range.edges(selection);
+
+    const startOfLine = Editor.before(editor, cursor, { unit: "line" });
+    if (!startOfLine) return;
+
+    const startOfLineToCursor = Editor.range(editor, startOfLine, cursor);
+    const s = Editor.string(editor, startOfLineToCursor);
+    if (!s) return;
+
+    const atPos = s.lastIndexOf("@");
+    if (atPos === -1) return;
+
+    const search = s.substring(atPos + 1);
+
+    const atLocation = Editor.before(editor, cursor, {
+      unit: "offset",
+      distance: search.length + 1,
+    });
+    if (!atLocation) return;
+
+    return { search, range: Editor.range(editor, atLocation, cursor) };
+  };
+
   return (
-    <NoSSR>
+    <>
       <h1>Rich Text Editor</h1>
       <div className={styles.compare_pane}>
         <h3>Editor</h3>
-        <Slate
-          editor={editor}
-          value={editorData}
-          onChange={(v) => {
-            setEditorData(v);
+        <NoSSR>
+          <Slate
+            editor={editor}
+            value={editorData}
+            onChange={(v) => {
+              setEditorData(v);
+              setMentionQuery(getMentionQueryFromEditor());
+            }}
+          >
+            <MarkdownEditable className={styles.editor} />
+          </Slate>
+        </NoSSR>
+      </div>
+      <div>
+        <button
+          disabled={!mentionQuery}
+          onClick={() => {
+            if (!mentionQuery) return;
+            Transforms.select(editor, mentionQuery.range);
+            Transforms.insertFragment(
+              editor,
+              markdownToEditorData(
+                "@USER-daa968aa-e7ad-4d99-98a6-c5d8a468b0e5 "
+              )
+            );
+            ReactEditor.focus(editor);
           }}
         >
-          <MarkdownEditable className={styles.editor} />
-        </Slate>
+          Add @
+        </button>
+        {mentionQuery?.search}
       </div>
-      <button
-        onClick={() => {
-          editor.insertFragment(
-            markdownToEditorData("@USER-daa968aa-e7ad-4d99-98a6-c5d8a468b0e5 ")
-          );
-          ReactEditor.focus(editor);
-        }}
-      >
-        Add @
-      </button>
 
       <div className={styles.compare_container}>
         <div className={styles.compare_pane}>
@@ -66,6 +111,6 @@ Nice!
           <MarkdownToHtml value={markdown} />
         </div>
       </div>
-    </NoSSR>
+    </>
   );
 }
